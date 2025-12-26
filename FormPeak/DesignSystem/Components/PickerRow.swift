5740607e02d9fe491bf75d1564fb74d7d9fe23a5
Artem <art.kriukov@gmail.com>
2025-12-26 16:43:06 +0300

    feat: add PickerRow button and impl it in factory

---
+++ FormPeak/DesignSystem/Components/PickerRow.swift
@@ -0,0 +1,97 @@
+import UIKit
+
+final class PickerRow: UIControl {
+    
+    private lazy var mainStackView = UIStackView()
+    private lazy var infoStackView = UIStackView()
+    private lazy var iconView = UIImageView()
+    private lazy var title = UILabel()
+    private lazy var chevron = UIImageView()
+    
+    init(with config: Configuration) {
+        super.init(frame: .zero)
+        setupPickerRow(with: config)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupPickerRow(with config: Configuration) {
+        self.backgroundColor = config.backgroundColor
+        self.tintColor = config.textColor
+        self.layer.cornerRadius = Layout.cornerMedium
+        
+        setupMainStackView()
+        setupInfoStackView()
+        setupTitle(with: config)
+        setupIconView(with: config)
+        setupChevron()
+        setupViews()
+        
+        addAction(UIAction { _ in
+            config.action()
+        }, for: .touchUpInside)
+    }
+    
+    private func setupTitle(with config: Configuration) {
+        title.text = config.title
+        title.textColor = AppColor.textSecondary
+        title.adjustsFontForContentSizeCategory = true
+        title.font = Typography.buttonSecondary
+    }
+    
+    private func setupMainStackView() {
+        mainStackView.axis = .horizontal
+        mainStackView.alignment = .center
+        mainStackView.distribution = .fill
+        mainStackView.spacing = Layout.spaceS
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupInfoStackView() {
+        infoStackView.axis = .horizontal
+        infoStackView.alignment = .center
+        infoStackView.spacing = Layout.spaceS
+    }
+    
+    private func setupIconView(with config: Configuration) {
+        iconView.image = config.icon
+    }
+    
+    private func setupChevron() {
+        chevron.image = UIImage(systemName: "chevron.right")
+    }
+    
+    private func setupViews() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(infoStackView)
+        infoStackView.addArrangedSubview(iconView)
+        infoStackView.addArrangedSubview(title)
+        mainStackView.addArrangedSubview(chevron)
+        
+        NSLayoutConstraint.activate([
+            iconView.widthAnchor.constraint(equalToConstant: 24),
+            iconView.heightAnchor.constraint(equalToConstant: 24),
+            
+            mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            mainStackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            
+            heightAnchor.constraint(equalToConstant: Layout.rowHeight)
+        ])
+    }
+}
+
+extension PickerRow {
+    struct Configuration {
+        let title: String
+        let icon: UIImage?
+        let backgroundColor: UIColor
+        let textColor: UIColor
+        let action: () -> Void
+    }
+}
--- FormPeak/DesignSystem/Factories/ButtonFactory.swift
+++ FormPeak/DesignSystem/Factories/ButtonFactory.swift
@@ -1,7 +1,3 @@
-//
-//  ButtonFactory.swift
-//
-
 import UIKit
 
 enum ButtonFactory {
@@ -22,4 +18,22 @@ enum ButtonFactory {
         return PrimaryButton(with: config)
     }
     
+    static func pickerRow(
+        title: String,
+        // TODO: после добавления иконок поменять дефолтное изображение
+        icon: UIImage? = UIImage(systemName: "poweroutlet.type.b.fill") ?? nil,
+        backgroundColor: UIColor = AppColor.surface,
+        textColor: UIColor = AppColor.textSecondary,
+        action: @escaping () -> Void
+    ) -> PickerRow {
+        let config = PickerRow.Configuration(
+            title: title,
+            icon: icon,
+            backgroundColor: backgroundColor,
+            textColor: textColor,
+            action: action
+        )
+        
+        return PickerRow(with: config)
+    }
 }
